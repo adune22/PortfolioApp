19b3620103017474065361143d56c004a3ea4e76
adune22 <97811167+adune22@users.noreply.github.com>
2022-04-22 14:48:53 -0600

    Final commit

---
+++ src/TechnologySummary.js
@@ -0,0 +1,34 @@
+function TechnologySummary() {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h2 className="card-title">Technology Summary</h2>
+                <ul className="list-unstyled">
+                    <li>
+                        <h6 className="fw-bold">Senior Web Developer / Digital Agency 2016-2020</h6>
+                        <p><ul>
+                            <li>React</li>
+                            <li>Node.js</li>
+                        </ul></p>
+                    </li>
+                    <li>
+                        <h6 className="fw-bold">Web Developer / Big Firm 2010-2015</h6>
+                        <p><ul>
+                            <li>Servlets</li>
+                            <li>JSP</li>
+                        </ul></p>
+                    </li>
+                    <li>
+                        <h6 className="fw-bold">Junior Web Developer / Websites 'R' Us 2004-2009</h6>
+                        <p><ul>
+                            <li>HTML, CSS, JS</li>
+                            <li>JSON</li>
+                        </ul></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default TechnologySummary;
